bb02310c306473a0f2ce4d47fa3b7c7a8c104939
m80126colin <m80126colin@gmail.com>
2015-11-17 22:59:05 +0800

    Fix for deleting files

--- images.js
+++
@@ -1,8 +0,0 @@
-module.exports.local = function (args) {
-  var path   = args[0];
-  var res = [];
-  res.push('<div class="owl-media owl-image owl-local-image">');
-  res.push('<img src="' + path + '" />');
-  res.push('</div>');
-  return res.join('');
-}
